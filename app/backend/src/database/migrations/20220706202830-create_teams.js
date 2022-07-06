922da92d9e121a09cfbdb03fa669884775e4d298
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-06 18:34:01 -0300

    feat:creates teams migration

---
+++ app/backend/src/database/migrations/20220706202830-create_teams.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.createTable('teams', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      team_name: {
+        type: Sequelize.STRING, 
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
