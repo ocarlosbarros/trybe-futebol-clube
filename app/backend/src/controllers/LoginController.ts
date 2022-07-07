82351636dcb5760e4b0022008b60e31c0abfe8f2
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 17:38:51 -0300

    feat:adds try catch in LoginController

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -8,10 +8,14 @@ class LoginController {
     this._authenticateUserService = authenticateUserService;
   }
 
-  public execute = async (request: Request, response: Response, _next: NextFunction) => {
-    const { email, password } = request.body;
-    const token = await this._authenticateUserService.execute(email, password);
-    return response.status(200).json({ token });
+  public execute = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { email, password } = request.body;
+      const token = await this._authenticateUserService.execute(email, password);
+      return response.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
