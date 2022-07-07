9ad20a17dc401959efb2f64dd47b8eb66d16aac5
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 16:38:23 -0300

    refactor:remove method in Utils and use process.env

--- app/backend/src/utils/index.ts
+++ app/backend/src/utils/index.ts
@@ -1,7 +1,8 @@
 import * as bcrypt from 'bcryptjs';
-import { readFile } from 'fs/promises';
 import * as jwt from 'jsonwebtoken';
 import IUser from '../database/models/interfaces/IUser';
+
+const SECRET = process.env.jwt_secret || 'jwt_secret';
 
 const encryptPassword = async (password: string): Promise<string> => {
   const SALT = 10;
@@ -16,30 +17,10 @@ const checkIsValidPassword = async (password: string, hash: string): Promise<boo
   return isValid;
 };
 
-const readFileFrom = async (path: string) => {
-  try {
-    const content = await readFile(path, 'utf-8');
-    return content;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getSecret = async () => {
-  try {
-    const secret = await readFileFrom('./jwt.evaluation.key');
-    return secret?.trim();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const generateToken = async (user: IUser) => {
-  const secret = await getSecret();
+  if (!SECRET) throw new Error('Authentication failed!');
 
-  if (!secret) throw new Error('Authentication failed!');
-
-  const token = jwt.sign(user, secret);
+  const token = jwt.sign(user, SECRET);
 
   return token;
 };
