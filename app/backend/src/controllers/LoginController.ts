4998623b9d8e41d9039c6633862149209b7dd5bd
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 19:21:18 -0300

    feat:creates validate method in LoginController

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import IRequest from '../@types/IRequest';
 import IAuthenticateUserService from '../services/interfaces/IAuthenticateUserService';
+import IUser from '../database/models/interfaces/IUser';
 
 class LoginController {
   private _authenticateUserService;
@@ -13,6 +15,15 @@ class LoginController {
       const { email, password } = request.body;
       const token = await this._authenticateUserService.execute(email, password);
       return response.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public validate = async (request: IRequest, response: Response, next: NextFunction) => {
+    try {
+      const { role } = request.user as IUser;
+      return response.status(200).json({ role });
     } catch (error) {
       next(error);
     }
