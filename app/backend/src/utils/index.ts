3d917803e99ea28fed155577c91e8640d3892e9a
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 13:41:44 -0300

    feat:creates methods generateToken and readFileFrom

--- app/backend/src/utils/index.ts
+++ app/backend/src/utils/index.ts
@@ -1,4 +1,7 @@
 import * as bcrypt from 'bcryptjs';
+import { readFile } from 'fs/promises';
+import * as jwt from 'jsonwebtoken';
+import IUser from '../database/models/interfaces/IUser';
 
 const encryptPassword = async (password: string): Promise<string> => {
   const SALT = 10;
@@ -13,7 +16,36 @@ const checkIsValidPassword = async (password: string, hash: string): Promise<boo
   return isValid;
 };
 
+const readFileFrom = async (path: string) => {
+  try {
+    const content = await readFile(path, 'utf-8');
+    return content;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSecret = async () => {
+  try {
+    const secret = await readFileFrom('./jwt.evaluation.key');
+    return secret?.trim();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateToken = async (user: IUser) => {
+  const secret = await getSecret();
+
+  if (!secret) throw new Error('Authentication failed!');
+
+  const token = jwt.sign(user, secret);
+
+  return token;
+};
+
 export {
   encryptPassword,
   checkIsValidPassword,
+  generateToken,
 };
