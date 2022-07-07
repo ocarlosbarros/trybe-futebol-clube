549893d61963eed2191141c0df7c598905e3bf48
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 19:24:39 -0300

    refactor:refactoring execute function in AuthenticateUserService

--- app/backend/src/services/AuthenticateUserService.ts
+++ app/backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { checkIsValidPassword, generateToken } from '../utils';
 import IUserRepository from '../database/models/repositories/IUserRepository';
 import IAuthenticateUserService from './interfaces/IAuthenticateUserService';
+import IUser from '../database/models/interfaces/IUser';
 
 class AuthenticateUserService implements IAuthenticateUserService {
   private _userRepository;
@@ -10,14 +11,15 @@ class AuthenticateUserService implements IAuthenticateUserService {
   }
 
   public async execute(email: string, password:string) {
-    const founded = await this._userRepository.findBy(email);
+    const founded = await this._userRepository.findBy(email) as IUser;
     const error = new Error('Incorrect email or password');
     error.name = 'Unauthorized';
 
     if (!founded) throw error;
 
-    const isValidPassword = await checkIsValidPassword(password, founded.password);
-    console.log(isValidPassword);
+    const { password: hash } = founded;
+
+    const isValidPassword = await checkIsValidPassword(password, hash);
 
     if (!isValidPassword) throw error;
 
