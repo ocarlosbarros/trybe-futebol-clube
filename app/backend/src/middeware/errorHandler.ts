c52fb37ed1faee6dcc874c9076a027c2cfca807d
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 18:25:29 -0300

    feat:adds case to Unauthorized error

--- app/backend/src/middeware/errorHandler.ts
+++ app/backend/src/middeware/errorHandler.ts
@@ -2,12 +2,16 @@ import { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 const handler = async (error: any, request: Request, response: Response, next: NextFunction) => {
-  const { name, details } = error;
-
+  const { name, details, message } = error;
+  console.log('name', name);
+  console.log('dls', details);
+  console.log('msn', message);
   switch (name) {
     case 'ValidationError': {
       return response.status(400).json({ message: details[0].message });
     }
+    case 'Unauthorized':
+      return response.status(401).json({ message });
     default:
       console.error(error);
       response.sendStatus(500);
