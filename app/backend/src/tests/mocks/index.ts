eff09369b0505353a59eedaabe0a4abfc026eead
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-07 17:08:01 -0300

    chore:change mock values

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -1,6 +1,6 @@
 export default {
-    username: 'Peter Parker',
-    role: 'admin',
-    email: 'parker@pete.com',
-    password: ' $2a$08$6jfM3shZXD53njg7wV9tQ.GuWuOgp.xMyddXXxJPRrN32/TvKVePm',
+    "username": "Admin",
+    "role": "admin",
+    "email": "admin@admin.com",
+    "password": "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW"
 }
