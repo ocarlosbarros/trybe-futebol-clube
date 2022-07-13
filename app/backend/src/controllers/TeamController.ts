385e383616d9a5bfbf2a5f4fb8c7c58fa97f7d00
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 11:45:04 -0300

    feat:creates findById in TeamController

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -16,6 +16,16 @@ class TeamController {
       next(error);
     }
   };
+
+  public findById = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { id } = request.params;
+      const founded = await this._teamService.findById(+id);
+      return response.status(200).json(founded);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default TeamController;
