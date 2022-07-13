9610b1452839306baddc3e2b35e51d125477d339
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 17:22:31 -0300

    refactor:adds error cases and remove console.log

--- app/backend/src/middeware/errorHandler.ts
+++ app/backend/src/middeware/errorHandler.ts
@@ -3,19 +3,27 @@ import 'express-async-errors';
 
 const handler = async (error: any, request: Request, response: Response, next: NextFunction) => {
   const { name, details, message } = error;
-  console.log('name', name);
-  console.log('dls', details);
-  console.log('msn', message);
+
+  if (message === 'It is not possible to create a match with two equal teams') {
+    return response.status(401).json({ message: details[0].message });
+  }
+
   switch (name) {
-    case 'ValidationError': {
+    case 'ValidationError':
       return response.status(400).json({ message: details[0].message });
-    }
+
     case 'Unauthorized':
       return response.status(401).json({ message });
+
+    case 'JsonWebTokenError':
+      return response.status(401).json({ message: 'Token must be a valid token' });
+
+    case 'SequelizeForeignKeyConstraintError':
+      return response.status(404).json({ message: 'There is no team with such id!' });
+
     default:
-      console.error(error);
-      response.sendStatus(500);
-      break;
+      console.error(message);
+      return response.status(500).end();
   }
   next();
 };
