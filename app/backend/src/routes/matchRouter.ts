ef3b0bb690b640c67667fc16690b2e3e5eaa73dc
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 17:21:48 -0300

    feat:creates matchValidate

--- app/backend/src/routes/matchRouter.ts
+++ app/backend/src/routes/matchRouter.ts
@@ -4,6 +4,7 @@ import ensureAuthenticated from '../middeware/ensureAuthenticated';
 import MatchRepository from '../database/models/repositories/MatchRepository';
 import MatchController from '../controllers/MatchController';
 import MatchService from '../services/MatchService';
+import matchValidate from '../middeware/matchValidate';
 
 const matchRepository = new MatchRepository();
 const matchService = new MatchService(matchRepository);
@@ -12,7 +13,7 @@ const matchController = new MatchController(matchService);
 const matchRouter = Router();
 
 matchRouter.get('/', matchController.findAll);
-matchRouter.post('/', ensureAuthenticated, matchController.create);
+matchRouter.post('/', ensureAuthenticated, matchValidate, matchController.create);
 matchRouter.patch('/:id/finish', ensureAuthenticated, matchController.patch);
 
 export default matchRouter;
