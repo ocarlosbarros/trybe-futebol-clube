efcb3a4c3ba77312ae554b5b92ca31b64ea47afb
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 10:56:31 -0300

    refactor:creates interface folder to repositories

--- app/backend/src/database/models/repositories/IRepository.ts
+++
@@ -1,7 +0,0 @@
-export default interface IRepository<T> {
-  create(): Promise<T>;
-  findAll(): Promise<T>;
-  findById(id: number): Promise<T>;
-  update(entity:T): Promise<T>;
-  destroy(id:number): Promise<void>;
-}
--- app/backend/src/database/models/repositories/IUserRepository.ts
+++
@@ -1,5 +0,0 @@
-import IUser from '../interfaces/IUser';
-
-export default interface IUserRepository {
-  findBy(email: string):Promise<IUser | null>
-}
