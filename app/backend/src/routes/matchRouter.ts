f96efdba59a9a5418b02c4f7cb8fd5d0aa22639b
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 13:07:52 -0300

    feat:adds post route to matches

--- app/backend/src/routes/matchRouter.ts
+++ app/backend/src/routes/matchRouter.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+
+import ensureAuthenticated from '../middeware/ensureAuthenticated';
 import MatchRepository from '../database/models/repositories/MatchRepository';
 import MatchController from '../controllers/MatchController';
 import MatchService from '../services/MatchService';
@@ -10,5 +12,6 @@ const matchController = new MatchController(matchService);
 const matchRouter = Router();
 
 matchRouter.get('/', matchController.findAll);
+matchRouter.post('/', ensureAuthenticated, matchController.create);
 
 export default matchRouter;
