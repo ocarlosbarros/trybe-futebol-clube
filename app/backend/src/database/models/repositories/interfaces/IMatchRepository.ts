77ed242fed77a39f8c6351cb819972f3fd02bdfb
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 16:30:38 -0300

    feat:adds methods in IMatchRepository and IMatchService

--- app/backend/src/database/models/repositories/interfaces/IMatchRepository.ts
+++ app/backend/src/database/models/repositories/interfaces/IMatchRepository.ts
@@ -3,4 +3,6 @@ import IMatch from '../../interfaces/IMatch';
 export default interface IMatchRepository {
   findAll():Promise<Array<IMatch> | null>
   create(match: IMatch):Promise<IMatch>
+  patch(match: IMatch):Promise<number>
+  findById(id: number):Promise<IMatch | null>
 }
--- app/backend/src/services/interfaces/IMatchService.ts
+++ app/backend/src/services/interfaces/IMatchService.ts
@@ -3,4 +3,5 @@ import IMatch from '../../database/models/interfaces/IMatch';
 export default interface IMatchService {
   findAll(): Promise<Array<IMatch> | null>;
   create(match: IMatch): Promise<IMatch>;
+  patch(id: number): Promise<number | null>;
 }
