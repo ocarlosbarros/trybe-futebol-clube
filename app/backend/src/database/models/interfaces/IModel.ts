2759e433790e0d01c790eb2647d1546f8e4cbddd
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 10:55:42 -0300

    refactor:delete IModel

--- app/backend/src/database/models/interfaces/IModel.ts
+++
@@ -1,8 +0,0 @@
-export default interface IModel<T> {
-  create(): Promise<T>;
-  findAll(): Promise<T>;
-  findOne(filter: string): Promise<T>;
-  findByPk(id: number): Promise<T>;
-  update(entity:T): Promise<T>;
-  destroy(id:number): Promise<void>;
-}
