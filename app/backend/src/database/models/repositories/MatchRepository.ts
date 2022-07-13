092916cbf620b09051c3cdb0fccfd02593eb378d
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-13 11:55:21 -0300

    feat:adds model teamHome and teamAway in method findAll

--- app/backend/src/database/models/repositories/MatchRepository.ts
+++ app/backend/src/database/models/repositories/MatchRepository.ts
@@ -1,5 +1,6 @@
 import IMatch from '../interfaces/IMatch';
 import Match from '../Match';
+import Team from '../Team';
 import IMatchRepository from './interfaces/IMatchRepository';
 
 class MatchRepository implements IMatchRepository {
@@ -10,7 +11,12 @@ class MatchRepository implements IMatchRepository {
   }
 
   async findAll(): Promise<Array<IMatch>> {
-    const allMatches = await this._model.findAll();
+    const allMatches = await this._model.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
     return allMatches;
   }
 }
