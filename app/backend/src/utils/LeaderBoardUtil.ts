5e86e7b96f3fae2ec4828e55d0f0253d3e0519d6
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-14 18:54:32 -0300

    feat:creates getTotalWins method

--- app/backend/src/utils/LeaderBoardUtil.ts
+++ app/backend/src/utils/LeaderBoardUtil.ts
@@ -8,3 +8,17 @@ const getTotalGames = (matches: Array<IMatch>) : number => {
   }, 0);
   return totalGames;
 };
+
+const getTotalWins = (matches: Array<IMatch>, team: ITeam) : number => {
+  let totalWins = 0;
+
+  matches.forEach((currentTeam) => {
+    if (team.id === currentTeam.homeTeam && currentTeam.homeTeamGoals > currentTeam.awayTeamGoals) {
+      totalWins += 1;
+    }
+    if (team.id === currentTeam.awayTeam && currentTeam.awayTeamGoals > currentTeam.homeTeamGoals) {
+      totalWins += 1;
+    }
+  });
+  return totalWins;
+};
